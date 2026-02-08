a861cf7ca986b720a91db0a209892a403ffad7c8
shivam9682 <ssivamyadav0123@gmail.com>
2026-02-08 07:29:41 +0530

    Update EmailService.java

--- HospitalmanagmentProject-1/src/main/java/in/sp/spring/service/EmailService.java
+++ HospitalmanagmentProject-1/src/main/java/in/sp/spring/service/EmailService.java
@@ -1,88 +1,33 @@
 package in.sp.spring.service;
 
-import java.util.Properties;
-
-//import javax.mail.Authenticator;
-//import javax.mail.Message;
-//import javax.mail.PasswordAuthentication;
-//import javax.mail.Session;
-//import javax.mail.Transport;
-//import javax.mail.internet.InternetAddress;
-//import javax.mail.internet.MimeMessage;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
-
-import jakarta.mail.Authenticator;
-import jakarta.mail.Message;
-import jakarta.mail.PasswordAuthentication;
-import jakarta.mail.Session;
-import jakarta.mail.Transport;
-import jakarta.mail.internet.InternetAddress;
-import jakarta.mail.internet.MimeMessage;
-
-
 
 @Service
 public class EmailService {
-	  
-	public boolean sendEmail(String subject , String message , String to ) {
-		boolean f=  false;
-		String from = "ssivamayadav0123@gmail.com";
-		// variable for email 
-		String host = "smtp.gmail.com";
-		 //get the system properties
-		 Properties properties = System.getProperties();
-		 System.out.println("PROPERTIES " +properties);
-		 
-		 
-		  // host set information  
-		 properties.put("mail.smtp.host",host);
-		 properties.put("mail.smtp.port","465");
-		 properties.put("mail.smtp.ssl.enable","true");
-		 properties.put("mail.smtp.auth","true");
-		  
-		  // step : 1 to get the session object 
-		 Session session = Session.getInstance(properties ,new Authenticator(){
-			   @Override
-			    protected PasswordAuthentication getPasswordAuthentication() {
-				   return new PasswordAuthentication("ssivamyadav0123@gmail.com","usfv hqjg jxdq rbjo");
-			   }
-			 
-		 });
-				  
-		 session.setDebug(true);
-		  
-		  // compose the media text to messaage ; 
-		  MimeMessage m  = new MimeMessage(session);
-		  try {
-			  // from email
-			   m.setFrom(from);
-			    //addind receimpt
-			    m.addRecipient(Message.RecipientType.TO , new InternetAddress(to));
-			    
-			    // adding subject 
-			   
-			    m.setSubject(subject);
-			     //adding text 
-			    m.setContent(message, "text/html; charset=UTF-8");
 
-			     // send message using transport class
-			      Transport.send(m);
-			      
-			       System.out.println("send message.........");
-			        f = true;
-			         
-			     
-		  }   catch(Exception e){
-			   e.printStackTrace();
-			  
-		  }
-		   return f;
-		   
-		 
-	}
+    @Autowired
+    private JavaMailSender mailSender;
 
-	
-	
+    public boolean sendEmail(String subject, String message, String to) {
+        boolean isSent = false;
 
+        try {
+            SimpleMailMessage mailMessage = new SimpleMailMessage();
+            mailMessage.setFrom("ssivamyadav0123@gmail.com"); // your email
+            mailMessage.setTo(to);
+            mailMessage.setSubject(subject);
+            mailMessage.setText(message);
+
+            mailSender.send(mailMessage);
+            System.out.println("Email sent successfully...");
+            isSent = true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return isSent;
+    }
 }
