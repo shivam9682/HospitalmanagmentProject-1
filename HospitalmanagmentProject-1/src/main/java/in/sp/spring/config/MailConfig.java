823df443fd584972cac3ed2c2057fcb03e5a9d01
shivam9682 <ssivamyadav0123@gmail.com>
2026-02-08 07:11:11 +0530

    Refactor EmailService to use SMTP properties

--- HospitalmanagmentProject-1/src/main/java/in/sp/spring/config/MailConfig.java
+++ HospitalmanagmentProject-1/src/main/java/in/sp/spring/config/MailConfig.java
@@ -1,62 +1,88 @@
 package in.sp.spring.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
+import java.util.Properties;
+
+//import javax.mail.Authenticator;
+//import javax.mail.Message;
+//import javax.mail.PasswordAuthentication;
+//import javax.mail.Session;
+//import javax.mail.Transport;
+//import javax.mail.internet.InternetAddress;
+//import javax.mail.internet.MimeMessage;
+
 import org.springframework.stereotype.Service;
 
+import jakarta.mail.Authenticator;
+import jakarta.mail.Message;
+import jakarta.mail.PasswordAuthentication;
+import jakarta.mail.Session;
+import jakarta.mail.Transport;
+import jakarta.mail.internet.InternetAddress;
 import jakarta.mail.internet.MimeMessage;
+
+
 
 @Service
 public class EmailService {
+	  
+	public boolean sendEmail(String subject , String message , String to ) {
+		boolean f=  false;
+		String from = "ssivamayadav0123@gmail.com";
+		// variable for email 
+		String host = "smtp.gmail.com";
+		 //get the system properties
+		 Properties properties = System.getProperties();
+		 System.out.println("PROPERTIES " +properties);
+		 
+		 
+		  // host set information  
+		 properties.put("mail.smtp.host",host);
+		 properties.put("mail.smtp.port","465");
+		 properties.put("mail.smtp.ssl.enable","true");
+		 properties.put("mail.smtp.auth","true");
+		  
+		  // step : 1 to get the session object 
+		 Session session = Session.getInstance(properties ,new Authenticator(){
+			   @Override
+			    protected PasswordAuthentication getPasswordAuthentication() {
+				   return new PasswordAuthentication("ssivamyadav0123@gmail.com","usfv hqjg jxdq rbjo");
+			   }
+			 
+		 });
+				  
+		 session.setDebug(true);
+		  
+		  // compose the media text to messaage ; 
+		  MimeMessage m  = new MimeMessage(session);
+		  try {
+			  // from email
+			   m.setFrom(from);
+			    //addind receimpt
+			    m.addRecipient(Message.RecipientType.TO , new InternetAddress(to));
+			    
+			    // adding subject 
+			   
+			    m.setSubject(subject);
+			     //adding text 
+			    m.setContent(message, "text/html; charset=UTF-8");
 
-    @Autowired
-    private JavaMailSender mailSender;
+			     // send message using transport class
+			      Transport.send(m);
+			      
+			       System.out.println("send message.........");
+			        f = true;
+			         
+			     
+		  }   catch(Exception e){
+			   e.printStackTrace();
+			  
+		  }
+		   return f;
+		   
+		 
+	}
 
-    /**
-     * Send plain OTP email
-     */
-    public boolean sendOtpEmail(String to, String otp) {
-        try {
-            String subject = "Your OTP Code";
-            String message = "Hello,\n\nYour OTP is: " + otp + "\n\nDo not share it with anyone.";
+	
+	
 
-            MimeMessage mimeMessage = mailSender.createMimeMessage();
-            MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, false, "utf-8");
-            helper.setText(message, false); // false = plain text
-            helper.setTo(to);
-            helper.setSubject(subject);
-            helper.setFrom(mailSender.getUsername());
-
-            mailSender.send(mimeMessage);
-            System.out.println("OTP email sent to " + to);
-            return true;
-        } catch (Exception e) {
-            e.printStackTrace();
-            return false;
-        }
-    }
-
-    /**
-     * Send HTML email
-     */
-    public boolean sendHtmlEmail(String to, String subject, String htmlContent) {
-        try {
-            MimeMessage mimeMessage = mailSender.createMimeMessage();
-            MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true, "utf-8");
-            helper.setTo(to);
-            helper.setSubject(subject);
-            helper.setText(htmlContent, true); // true = HTML
-            helper.setFrom(mailSender.getUsername());
-
-            mailSender.send(mimeMessage);
-            System.out.println("HTML email sent to " + to);
-            return true;
-        } catch (Exception e) {
-            e.printStackTrace();
-            return false;
-        }
-    }
 }
-
-
