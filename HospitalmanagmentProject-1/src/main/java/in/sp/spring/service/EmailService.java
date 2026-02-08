36aa2a8209dcba1e26db8aa1e7ce6151f44e1ded
shivam9682 <ssivamyadav0123@gmail.com>
2026-02-08 07:16:49 +0530

    Update EmailService to use Jakarta Mail for email sending
    Refactored EmailService to use Jakarta Mail API and added SMTP configuration.

--- HospitalmanagmentProject-1/src/main/java/in/sp/spring/service/EmailService.java
+++ HospitalmanagmentProject-1/src/main/java/in/sp/spring/service/EmailService.java
@@ -4,7 +4,7 @@ import java.util.Properties;
 
 //import javax.mail.Authenticator;
 //import javax.mail.Message;
-//import javax.mail.PsswordAuthentication;
+//import javax.mail.PasswordAuthentication;
 //import javax.mail.Session;
 //import javax.mail.Transport;
 //import javax.mail.internet.InternetAddress;
@@ -20,31 +20,69 @@ import jakarta.mail.Transport;
 import jakarta.mail.internet.InternetAddress;
 import jakarta.mail.internet.MimeMessage;
 
+
+
 @Service
 public class EmailService {
+	  
+	public boolean sendEmail(String subject , String message , String to ) {
+		boolean f=  false;
+		String from = "ssivamayadav0123@gmail.com";
+		// variable for email 
+		String host = "smtp.gmail.com";
+		 //get the system properties
+		 Properties properties = System.getProperties();
+		 System.out.println("PROPERTIES " +properties);
+		 
+		 
+		  // host set information  
+		 properties.put("mail.smtp.host",host);
+		 properties.put("mail.smtp.port","465");
+		 properties.put("mail.smtp.ssl.enable","true");
+		 properties.put("mail.smtp.auth","true");
+		  
+		  // step : 1 to get the session object 
+		 Session session = Session.getInstance(properties ,new Authenticator(){
+			   @Override
+			    protected PasswordAuthentication getPasswordAuthentication() {
+				   return new PasswordAuthentication("ssivamyadav0123@gmail.com","usfv hqjg jxdq rbjo");
+			   }
+			 
+		 });
+				  
+		 session.setDebug(true);
+		  
+		  // compose the media text to messaage ; 
+		  MimeMessage m  = new MimeMessage(session);
+		  try {
+			  // from email
+			   m.setFrom(from);
+			    //addind receimpt
+			    m.addRecipient(Message.RecipientType.TO , new InternetAddress(to));
+			    
+			    // adding subject 
+			   
+			    m.setSubject(subject);
+			     //adding text 
+			    m.setContent(message, "text/html; charset=UTF-8");
 
-    @Autowired
-    private JavaMailSender mailSender;
+			     // send message using transport class
+			      Transport.send(m);
+			      
+			       System.out.println("send message.........");
+			        f = true;
+			         
+			     
+		  }   catch(Exception e){
+			   e.printStackTrace();
+			  
+		  }
+		   return f;
+		   
+		 
+	}
 
-    public boolean sendEmail(String subject, String message, String to) {
-        boolean success = false;
+	
+	
 
-        try {
-            SimpleMailMessage mailMessage = new SimpleMailMessage();
-            mailMessage.setTo(to);
-            mailMessage.setSubject(subject);
-            mailMessage.setText(message);
-
-            // Spring Boot automatically uses Railway env vars
-            mailSender.send(mailMessage);
-
-            System.out.println("Email sent successfully!");
-            success = true;
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        return success;
-    }
 }
-
