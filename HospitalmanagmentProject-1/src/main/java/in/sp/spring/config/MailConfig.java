7b80dd87f4ee2586eac2f156bca154c1f04a1eec
shivam9682 <ssivamyadav0123@gmail.com>
2026-02-08 06:59:49 +0530

    Add @Configuration annotation to MailConfig

--- HospitalmanagmentProject-1/src/main/java/in/sp/spring/config/MailConfig.java
+++ HospitalmanagmentProject-1/src/main/java/in/sp/spring/config/MailConfig.java
@@ -1,9 +1,11 @@
 package in.sp.spring.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 
+@Configuration
 public class MailConfig {
 
     @Bean
